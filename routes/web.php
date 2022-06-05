f0ea1ff3575ca9d5de542c192189ba721fe93ff6
jirsh <jirsh@users.noreply.github.com>
2022-06-05 22:36:56 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,20 @@ use App\Http\Controllers\TicketsController;
 use App\Http\Controllers\RepliesController;
 use App\Http\Controllers\AttachmentController;
 
-Route::get('/', [HomepageController::class, 'index'])->name('home');
+Route::get('/', [HomepageController::class, 'index'])
+    ->name('home');
 
-Route::get('/tickets/{id}', [TicketsController::class, 'show'])->name('ticket.show');
+Route::get('/tickets/{id}', [TicketsController::class, 'show'])
+    ->whereNumber('id')
+    ->name('ticket.show');
 
-Route::post('/tickets', [TicketsController::class, 'store'])->name('tickets.new');
+Route::post('/tickets', [TicketsController::class, 'store'])
+    ->name('tickets.new');
 
-Route::post('/reply/{ticketId}', [RepliesController::class, 'store'])->name('reply.new');
+Route::post('/reply/{ticketId}', [RepliesController::class, 'store'])
+    ->whereNumber('ticketId')
+    ->name('reply.new');
 
-Route::get('/attachment/{id}', [AttachmentController::class, 'download'])->name('attachment.download');
+Route::get('/attachment/{id}', [AttachmentController::class, 'download'])
+    ->whereNumber('id')
+    ->name('attachment.download');
